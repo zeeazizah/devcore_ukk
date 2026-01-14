c4948f5721dcedf137fd4a353ab93cef57f42912
zeeazizah <azizahnurqamariah0904@gmail.com>
2026-01-14 09:04:59 +0800

    second commit index.php

--- config.php
+++ config.php
@@ -9,9 +9,4 @@
     // Create connection
     $conn = mysqli_connect($host, $username, $password, $database);
 
-    // Check connection
-    if (!$conn) {
-        die("Database connection failed: " . mysqli_connect_error());
-    }
-
 ?>
